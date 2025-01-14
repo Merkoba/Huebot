ac72206d150fd1482919cccd26e0f8891d35b40d
Auric Vente <mprops@protonmail.com>
2025-01-14 15:13:34 -0600

    Docs

---
+++ pm2_huebot.config.js
@@ -0,0 +1,42 @@
+const OPENAI_KEY = `someKey`
+const GOOGLE_KEY = `someKey`
+
+const BOTS = [
+  `john`,
+  `paul`,
+  `george`,
+  `ringo`,
+]
+
+const COMMON = {
+  script: 'huebot/src/huebot.js',
+  instances: 1,
+  autorestart: true,
+  watch: false,
+  max_memory_restart: `1G`,
+  env: {
+    NODE_ENV: `production`,
+    OPENAI_API_KEY: OPENAI_KEY,
+    GOOGLE_API_KEY: GOOGLE_KEY,
+  },
+  env_production: {
+    NODE_ENV: `production`,
+  },
+}
+
+function bot (name) {
+  return Object.assign({}, COMMON, {
+    name: `huebot_${name}`,
+    args: `${name} ${name}`,
+  })
+}
+
+const apps = []
+
+for (const name of BOTS) {
+  apps.push(bot(name))
+}
+
+module.exports = {
+  apps,
+}
